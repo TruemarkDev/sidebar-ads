5ece328ac42077141a2b0a2ea7731602f5cdaed2
Prakash Poudel <prakashpoudel@lftechnology.com>
2017-04-23 17:10:46 +0545

    submit form to google sheet

---
+++ polar_ads/contact_polar/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+    // Here is how to show an error message next to a form field
+
+    // var errorField = $('.form-input-name-row');
+    //
+    // // Adding the form-invalid-data class will show
+    // // the error message and the red x for that field
+    //
+    // errorField.addClass('form-invalid-data');
+    // errorField.find('.form-invalid-data-info').text('Please enter your name');
+    //
+    //
+    // // Here is how to mark a field with a green check mark
+    //
+    // var successField = $('.form-input-email-row');
+    //
+    // successField.addClass('form-valid-data');
+    //
+    //
+    // var $form = $("#ad-with-us");
+    var checkBeforeSubmit = function () {
+
+    }
+});
